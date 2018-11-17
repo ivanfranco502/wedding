41d52f8fc9084de21dbae3cafbf746b7bc480a82
Ivan Franco <ifranco@tupaca.com>
2018-11-17 16:55:51 -0300

    wedding website

---
+++ send_form.php
@@ -0,0 +1,35 @@
+<?php 
+//Add here the email where you want to recieve data sent from the form.
+$your_email="ivanfranco502@gmail.com";
+
+$result = '<img src="img/form-icon-error.png"/><br/><span class="title error">Sorry!</span><br/>Your data has not been sent. Please try again.<br /><strong>Error: #AJ002</strong><br /><br /><button class="btn btn-default BtnCloseResult" type="button">Close</button>';
+if(!empty($_POST)){
+    
+    $events = '';
+    if(isset($_POST['events']) && $_POST['events'] != ''){
+        foreach($_POST['events'] as $value){
+            $events .= ''.$value.', ';
+        }
+    }
+    $menues = '';
+    if(isset($_POST['menues']) && $_POST['menues'] != ''){
+        foreach($_POST['menues'] as $value){
+            $menues .= ''.$value.', ';
+        }
+    }
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    
+    $to      = $your_email;
+    $subject = 'New RSVP Form: '.$name;
+    $headers = 'From: '.$name.' <'.$email.'>' . "\r\n";
+    $message = 'New RSVP data sent from your website:'."\r\n".'Name: '.$name."\r\n".'Email: '.$email."\r\n".'Will attend: '.$events."\r\n".'Will eat: '.$menues."\r\n".'Message: '.$message;
+    
+    if(mail($to, $subject, $message, $headers)){
+        $result = '<img src="img/form-icon-ok.png"/><br /><span class="title success">Gracias!</span><br>Su confirmación ha sido enviada!<br /><br /><button class="btn btn-default BtnCloseResult" type="button">Cerrar</button>';
+    }
+}
+
+echo $result;
+?>
